c9a196350933669636cb80a24dc663039d2336f0
Tolsager <victortolsagerolesen1@gmail.com>
2024-06-06 13:50:55 +0200

    refactor

--- exercises/day1/lcg.py
+++ exercises/day1/lcg.py
@@ -1,28 +1,9 @@
 import random
 import matplotlib.pyplot as plt
+from stat_tests import chi_square_test
 
 from scipy.stats import chi2
 
-
-def chi_square_test(n_bins, random_numbers):
-    random_numbers = sorted(random_numbers)
-    n = len(random_numbers)
-    bin_size = n // n_bins
-    bins = []
-    for i in range(n_bins):
-        bins.append(random_numbers[i * bin_size:(i + 1) * bin_size])
-
-    expected = n / n_bins
-    chi_square = 0
-    for i in range(n_bins):
-        chi_square += (len(bins[i]) - expected)**2 / expected
-
-    return chi_square
-
-
-
-def kolmogorov_smirnov_test():
-    pass
 
 
 def lcg(seed, multiplier, shift, modulus, size):
