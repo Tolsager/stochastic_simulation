1ade17b299be2f7b945b4c5b358edafc65d484fb
RSM-git <rasmus.steen.mikkelsen@hotmail.com>
2024-06-06 13:57:07 +0200

    cleaned chi_square test some lcg stuff

--- exercises/day1/lcg.py
+++ exercises/day1/lcg.py
@@ -5,12 +5,12 @@ from stat_tests import chi_square_test
 from scipy.stats import chi2
 
 
-
 def lcg(seed, multiplier, shift, modulus, size):
-    random_numbers = []
-    for i in range(size):
-        seed = (multiplier * seed + shift) % modulus
-        random_numbers.append(seed / modulus)
+    random_numbers = [(seed := (multiplier * seed + shift) % modulus) / modulus for _ in range(size)]
+    # random_numbers = []
+    # for i in range(size):
+    #     seed = (multiplier * seed + shift) % modulus
+    #     random_numbers.append(seed / modulus)
 
     return random_numbers
 
@@ -22,9 +22,18 @@ if __name__ == '__main__':
     modulus = 2**32
     size = 10000
     random_numbers = lcg(seed, multiplier, shift, modulus, size)
-    print(random_numbers)
 
     plt.hist(random_numbers, bins=10, edgecolor='black', width=0.09)
     plt.show()
 
     test_stat = chi_square_test(10, random_numbers)
+    print(test_stat)
+
+    # Degrees of freedom = n_bins - 1
+    dof = 10 - 1
+    # Significance level
+    alpha = 0.05
+    critical_value = chi2.ppf(1 - alpha, dof)
+
+    print(critical_value > test_stat)
+    
