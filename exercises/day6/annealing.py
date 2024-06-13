ec05bf9672d19ccb764486fcb106c7ed35aa59ef
Tolsager <victortolsagerolesen1@gmail.com>
2024-06-13 15:15:15 +0200

    started annealing and finished the MC function

--- exercises/day6/annealing.py
+++ exercises/day6/annealing.py
@@ -1,37 +1,45 @@
 import numpy as np
+import numpy.typing as npt
+from typing import *
 
 
 class Annealing:
-    def __init__(self, n_iterations: int, m: int, x0: int, n: int):
+    def __init__(self, n_iterations: int, x0: int, n: int, T0: float, A: npt.NDArray):
         self.n_iterations = n_iterations
-        self.m = m
         self.x0 = x0
         self.n = n
-        
+        self.T0 = T0
+        self.A = A
 
-    def h(self, x: int):
+    def h(self, x: Iterable):
         to_swap = np.random.choice(np.arange(1, self.n), size=2, replace=False)
         temp = x[to_swap[0]]
         x[to_swap[0]] = x[to_swap[1]]
         x[to_swap[1]] = temp
         return x
     
-    def g(self, x: float):
-        return pow(self.A, x) / math.factorial(x)
-     
+    def f(self, x: Iterable):
+        total_cost = 0
+        for i in range(self.n):
+            total_cost += self.A[x[i], x[i+1]]
+        return total_cost
+    
     def metropolis(self):
         x = self.x0
         samples = [x]
+        T = self.T0
         for i in range(self.n_iterations-1):
-            delta = self.h(x)
-            proposal = delta
+            proposal = self.h(x)
+            accept_p = min(1, np.exp(-(self.f(proposal) - self.f(x)))/T)
 
-            U = np.random.rand()
-
-            accept_p = np.minimum(1, self.g(proposal) / self.g(x))
-            
+            U = np.random.rand()            
             if U < accept_p:
                 x = proposal
             
             samples.append(x)
+        
+            T = 1 / np.sqrt(2 + i)
         return samples
+
+if __name__ == "__main__":
+
